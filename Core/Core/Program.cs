36ce09ec2032b9fd5b004b8cd6ef0fedf2b36874
dhernandez <danielhernandez1718@outlook.es>
2016-11-07 19:11:07 +0000

    Se agrega NLog

--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Core
+﻿using NLog;
+using System;
+
+namespace Core
 {
     public class Program
     {
+        public Logger Logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Logger.Info("Core... Desde NLog");
         }
     }
 }
